b2adca052b134faffeaee7dd92ad3fb7cf6908dc
digbose92 <digbose92@gmail.com>
2022-12-20 18:05:12 -0800

    Added minimal updates to the bottleneck tokens addition code

--- src/transformers/models/vilt/modeling_vilt.py
+++ src/transformers/models/vilt/modeling_vilt.py
@@ -106,6 +106,10 @@ class ViltEmbeddings(nn.Module):
         self.patch_embeddings = ViltPatchEmbeddings(config)
         num_patches = self.patch_embeddings.num_patches
         self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches + 1, config.hidden_size))
+
+        #initialize the bottleneck embedding layer
+        self.bottleneck=nn.Embedding(config.num_bottleneck_tokens,config.hidden_size)
+
         # modality type (text/patch) embeddings
         self.token_type_embeddings = nn.Embedding(config.modality_type_vocab_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
@@ -234,11 +238,23 @@ class ViltEmbeddings(nn.Module):
             torch.full_like(image_masks, image_token_type_idx, dtype=torch.long, device=text_embeds.device)
         )
 
-        # PART 4: concatenate
-        embeddings = torch.cat([text_embeds, image_embeds], dim=1)
-        masks = torch.cat([attention_mask, image_masks], dim=1)
+        #bottleneck embeddings extract from bottleneck layer and expand 
+        #to the same size as text_embeds and image_embeds
+        bottleneck_masks=torch.ones(image_embeds.shape[0],self.config.num_bottleneck_tokens)
+        bottleneck_embeds=self.bottleneck_embeddings(torch.arange(self.config.num_bottleneck_tokens).unsqueeze(0).repeat(image_embeds.shape[0],1))
+        
+        
+        # PART 4: concatenate the embeddings
+        bn_image_embeddings = torch.cat([bottleneck_embeds, image_embeds], dim=1)
+        bn_text_embeddings = torch.cat([text_embeds,bottleneck_embeds], dim=1)
 
-        return embeddings, masks
+
+        #masks of bottleneck and image and text combinations
+        bn_image_masks = torch.cat([bottleneck_masks, image_masks], dim=1)
+        bn_text_masks= torch.cat([attention_mask,bottleneck_masks], dim=1)
+
+        #return all the combinations of embeddings and masks 
+        return text_embeds, image_embeds, bn_image_embeddings, bn_text_embeddings, bn_image_masks, bn_text_masks
 
 
 class TextEmbeddings(nn.Module):
@@ -845,11 +861,9 @@ class ViltModel(ViltPreTrainedModel):
             image_embeds,
             image_token_type_idx=image_token_type_idx,
         )
-
         # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
         # ourselves in which case we just need to make it broadcastable to all heads.
         extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape)
-
         encoder_outputs = self.encoder(
             embedding_output,
             attention_mask=extended_attention_mask,
@@ -858,6 +872,15 @@ class ViltModel(ViltPreTrainedModel):
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
         )
+        
+        # encoder_outputs = self.encoder(
+        #     embedding_output,
+        #     attention_mask=extended_attention_mask,
+        #     head_mask=head_mask,
+        #     output_attentions=output_attentions,
+        #     output_hidden_states=output_hidden_states,
+        #     return_dict=return_dict,
+        # )
         sequence_output = encoder_outputs[0]
         sequence_output = self.layernorm(sequence_output)
         pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
@@ -871,6 +894,164 @@ class ViltModel(ViltPreTrainedModel):
             hidden_states=encoder_outputs.hidden_states,
             attentions=encoder_outputs.attentions,
         )
+
+
+### ViLTModel (not initializing with pretreined weights)
+class ViltBNModel(nn.Module):
+    def __init__(self, config, add_pooling_layer=True):
+        super().__init__(config)
+        self.config = config
+
+        self.embeddings = ViltEmbeddings(config)
+        self.encoder = ViltEncoder(config)
+
+        self.layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.pooler = ViltPooler(config) if add_pooling_layer else None
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_input_embeddings(self):
+        return self.embeddings.text_embeddings.word_embeddings
+
+    def set_input_embeddings(self, value):
+        self.embeddings.text_embeddings.word_embeddings = value
+
+    def _prune_heads(self, heads_to_prune):
+        """
+        Prunes heads of the model. heads_to_prune: dict of {layer_num: list of heads to prune in this layer} See base
+        class PreTrainedModel
+        """
+        for layer, heads in heads_to_prune.items():
+            self.encoder.layer[layer].attention.prune_heads(heads)
+
+    @add_start_docstrings_to_model_forward(VILT_INPUTS_DOCSTRING)
+    @replace_return_docstrings(output_type=BaseModelOutputWithPooling, config_class=_CONFIG_FOR_DOC)
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        pixel_values: Optional[torch.FloatTensor] = None,
+        pixel_mask: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        image_embeds: Optional[torch.FloatTensor] = None,
+        image_token_type_idx: Optional[int] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ):
+        r"""
+        Returns:
+
+        Examples:
+
+        ```python
+        >>> from transformers import ViltProcessor, ViltModel
+        >>> from PIL import Image
+        >>> import requests
+
+        >>> # prepare image and text
+        >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        >>> image = Image.open(requests.get(url, stream=True).raw)
+        >>> text = "hello world"
+
+        >>> processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-mlm")
+        >>> model = ViltModel.from_pretrained("dandelin/vilt-b32-mlm")
+
+        >>> inputs = processor(image, text, return_tensors="pt")
+        >>> outputs = model(**inputs)
+        >>> last_hidden_states = outputs.last_hidden_state
+        ```"""
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+        elif inputs_embeds is not None:
+            input_shape = inputs_embeds.size()[:-1]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+
+        text_batch_size, seq_length = input_shape
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+
+        if attention_mask is None:
+            attention_mask = torch.ones(((text_batch_size, seq_length)), device=device)
+
+        if pixel_values is not None and image_embeds is not None:
+            raise ValueError("You cannot specify both pixel_values and image_embeds at the same time")
+        elif pixel_values is None and image_embeds is None:
+            raise ValueError("You have to specify either pixel_values or image_embeds")
+
+        image_batch_size = pixel_values.shape[0] if pixel_values is not None else image_embeds.shape[0]
+        if image_batch_size != text_batch_size:
+            raise ValueError("The text inputs and image inputs need to have the same batch size")
+        if pixel_mask is None:
+            pixel_mask = torch.ones((image_batch_size, self.config.image_size, self.config.image_size), device=device)
+
+        # Prepare head mask if needed
+        # 1.0 in head_mask indicate we keep the head
+        # attention_probs has shape bsz x n_heads x N x N
+        # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
+        # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
+        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
+
+        text_embeds, image_embeds, bn_image_embeddings, bn_text_embeddings, bn_image_masks, bn_text_masks = self.embeddings(
+            input_ids,
+            attention_mask,
+            token_type_ids,
+            pixel_values,
+            pixel_mask,
+            inputs_embeds,
+            image_embeds,
+            image_token_type_idx=image_token_type_idx,
+        )
+
+        # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
+        # ourselves in which case we just need to make it broadcastable to all heads.
+        extended_bn_image_attention_mask: torch.Tensor = self.get_extended_attention_mask(bn_image_masks, input_shape)
+        extended_bn_text_attention_mask: torch.Tensor = self.get_extended_attention_mask(bn_text_masks, input_shape)
+        #first forward pass (bn + imkage combination)
+
+        bn_image_encoder_outputs = self.encoder(
+            bn_image_embeddings,
+            attention_mask=extended_bn_image_attention_mask,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        #second forward pass (bn + text combination)
+        bn_text_encoder_outputs = self.encoder(
+            bn_text_embeddings,
+            attention_mask=extended_bn_text_attention_mask,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        #extract the bottleneck segments fromm bn_text_encoder_outputs by using specific number of tokens 
+        text_feats, bn_feats = (
+            bn_text_encoder_outputs.last_hidden_state[:, :text_embeds.shape[1],:],
+            bn_text_encoder_outputs.last_hidden_state[:, text_embeds.shape[1]:,:],
+        )
+
+        #average pool the bn outputs 
+        pooled_output = bn_feats.mean(dim=1)
+        sequence_output = self.layernorm(bn_feats)
+        #pooled_output = self.pooler(bn_feats) if self.pooler is not None else None
+
+        return (sequence_output)
+
 
 
 class ViltPooler(nn.Module):
